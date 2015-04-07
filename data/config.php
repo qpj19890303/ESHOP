30fd68dce583eb54ada865858754ef65905ead2f
qpj19890303 <532776178@qq.com>
2015-04-07 22:37:08 +0800

    -a

---
+++ data/config.php
@@ -0,0 +1,88 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "21cake";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "";
+
+// table prefix
+$prefix    = "jindong_";
+
+$timezone    = "Asia/Shanghai";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$admin_dir = "admin";
+
+$session = "1440";
+
+define('EC_CHARSET', 'utf-8');
+
+
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+
+
+
+
+
+
+define('API_TIME', '2015-03-28 22:35:52');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
